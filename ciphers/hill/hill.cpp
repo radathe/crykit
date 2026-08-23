31d778c318c35d13800aeeea62b4a48930170be9
radathe <dana.eltsova@gmail.com>
2026-08-23 17:54:36 +0700

    исправленная структура, сигнатуры функций

---
+++ ciphers/hill/hill.cpp
@@ -0,0 +1,4 @@
+#include "hill.hpp"
+#include "utils.hpp"
+#include "crykit.hpp"
+#include <cmath>
---
+++ include/crykit.hpp
@@ -0,0 +1,22 @@
+
+
+struct ConstBuffer {
+    const uint8_t* data;
+    size_t size;
+};
+
+struct MutBuffer {
+    uint8_t* data;
+    size_t size;
+};
+
+struct AlgorithmInfo {
+    const char * algorithm_name;
+    size_t key_size;
+};
+extern "C" const AlgorithmInfo* get_algorithm_info();
+
+extern "C" size_t get_output_size(size_t input_size, int operation_type);
+
+extern "C" int encrypt(ConstBuffer key, ConstBuffer input, MutBuffer* output);
+extern "C" int decrypt(ConstBuffer key, ConstBuffer input, MutBuffer* output);
