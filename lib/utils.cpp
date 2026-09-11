9709e4ce6ab5fed2c08dd60e6b2867966f2328bf
radathe <dana.eltsova@gmail.com>
2026-09-11 19:33:11 +0700

    сохраняю паддинг для добавления в main выделения памяти под него

--- lib/utils.cpp
+++ lib/utils.cpp
@@ -1,10 +1,18 @@
 #include "utils.hpp"
 #include "crykit.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <stdexcept>
 
 namespace utils{
-    ConstBuffer do_padding(ConstBuffer input, size_t block_size){
-        ConstBuffer padded{
-
+    void do_padding(ConstBuffer input, size_t block_size, MutBuffer& output){
+        if (block_size == 0 || block_size > 255) {
+            throw std::invalid_argument("PKCS#7 padding: block_size must be in range [1, 255]");
         }
+        size_t data_len = block_size-(input.size%block_size)+input.size;
+        uint8_t padded_array[data_len] {0};
+
+
+
     }
 }
